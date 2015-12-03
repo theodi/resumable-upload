7c228eb8eb91bea3f76cebc24f1d4acd55403929
pezholio <pezholio@gmail.com>
2015-12-03 15:15:55 +0000

    Add region

--- lib/fog_storage.rb
+++ lib/fog_storage.rb
@@ -10,7 +10,8 @@ class FogStorage
     @connection = Fog::Storage.new({
       :aws_access_key_id => ENV['AWS_ACCESS_KEY'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :provider => "AWS"
+      :provider => "AWS",
+      :region => "eu-west-1"
     })
   end
 
